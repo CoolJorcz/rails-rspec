2ae89e3322f9544ced39612b410ff51e2a4689ea
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-31 17:26:14 -0500

    Added slug helper

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :content, :is_published
 
+
   scope :recent, order: "created_at DESC", limit: 5
 
-  before_save :titleize_title
+  before_save :titleize_title,
 
   validates_presence_of :title, :content
 
@@ -12,4 +13,14 @@ class Post < ActiveRecord::Base
   def titleize_title
     self.title = title.titleize
   end
+
+  def slugalize(string)
+    string.gsub!(/[^\x00-\x7F]+/, '')
+    string.gsub!(/[^a-z0-9\-_\+]+/, '-')
+    string.gsub!(/-{2,}/, '-')
+    string.gsub!(/^-|-$/, '')
+    string.downcase!
+    string
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423183943) do
+ActiveRecord::Schema.define(:version => 20130831222246) do
 
   create_table "posts", :force => true do |t|
     t.string   "slug"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(:version => 20130423183943) do
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
   end
+
+  add_index "posts", ["slug"], :name => "index_posts_on_slug"
 
 end
